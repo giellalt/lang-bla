0c0263565228486c08a06c6c2e0f872fd014279b
antti.arppe@iki.fi <antti.arppe@iki.fi>
2022-12-27 21:20:49 -0600

    Added option to create HFST (and eventually HFSTOL) versions of the rewrite rule FSTs; in principle, HFSTOL versions can be compiled, but they do not seem to work for now; Added also some initial checks on the arguments (same as in 'rewrite-rule-check.sh'.

--- inc/bla-ruletest/compile-rewrite-rules.sh
+++ inc/bla-ruletest/compile-rewrite-rules.sh
@@ -1,12 +1,29 @@
 #!/bin/sh
 
-# compile-rewrite-rules.sh 1: XFSCRIPT (path to)
+# compile-rewrite-rules.sh 1: XFSCRIPT (path to) 2: FSTTYPE
 
-# echo /Users/arppe/giella/langs/crk/src/fst/phonology.xfscript |
+# echo /Users/arppe/giella/langs/crk/src/fst/phonology.xfscript foma |
 
 echo '' |
     
-gawk -v XFSCRIPT=$1 'BEGIN { xfscript=XFSCRIPT; 
+gawk -v XFSCRIPT=$1 -v FSTTYPE=$2 'BEGIN { xfscript=XFSCRIPT; fsttype=FSTTYPE;
+
+# if(fsttype!="foma" && fsttype!="hfst" && fsttype!="hfstol")
+if(fsttype!="foma" && fsttype!="hfst")
+  {
+    if(fsttype=="")
+      {
+        fsttype="foma";
+        print "Setting FST type as \"foma\" by default" > "/dev/stderr";
+      }
+    else
+      {
+        # print "Aborting <- specify FST type for phonological rule(s) among the following: 1) foma; 2) hfst; or 3) hfstol";
+        print "Aborting <- specify FST type for phonological rule(s) among the following: 1) foma; or 2) hfst";
+        exit;
+      }
+  }
+
 while((getline < xfscript)!=0)
   { if(index($0,"regex")!=0)
       { rx=1; regex=""; }
@@ -20,6 +37,22 @@ sub("[ ]*\\].*;.*$","",regex);
 n=split(regex,rule,"[ ]*\\.o\\.[ ]*");
 cmd="-e \"source "xfscript"\"";
 for(i=1; i<=n; i++)
-   cmd=cmd" -e \"clear stack\" -e \"push "rule[i]"\" -e \"save stack "rule[i]".foma\"";
+   cmd=cmd" -e \"clear stack\" -e \"push "rule[i]"\" -e \"save stack "rule[i]"."fsttype"\"";
 }
-END { system("foma "cmd" -e \"quit\""); }'
+
+END {
+if(fsttype=="foma")
+  system("foma "cmd" -e \"quit\"");
+if(fsttype=="hfst" || fsttype=="hfstol")
+  system("hfst-xfst "cmd" -e \"quit\"");
+# The following conversion works in principle, but the resultant HFSTOL FSTs do now work
+if(fsttype=="hfstol")
+  {
+    for(i=1; i<=n; i++)
+       {
+         hfst=rule[i]".hfst";
+         hfstol=rule[i]".hfstol";
+         system("hfst-fst2fst -O -i " hfst " -o " hfstol ";");
+       }
+  }
+}'
