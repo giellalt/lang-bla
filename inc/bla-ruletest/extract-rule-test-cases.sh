0a93a475fdf73399fb646c3184f3d1e55946a025
antti.arppe@iki.fi <antti.arppe@iki.fi>
2023-02-03 19:58:49 -0700

    Created script for extracting test cases marked with 'git log extract-rule-test-cases.sh€' from the file normally named: 'phonology.xfscript'; generalized behavior so that marker can be set, but default used if not provided; implemented testing of the existence of the phonology file.

--- inc/bla-ruletest/extract-rule-test-cases.sh
+++ inc/bla-ruletest/extract-rule-test-cases.sh
@@ -1,29 +1,36 @@
 #!/bin/sh
 
 # Usage:
-#    ./extract-rule-test-cases.sh 1:PATH-TO-phonology.xfscript
+#    ./extract-rule-test-cases.sh 1:PATH-TO-phonology.xfscript 2:MARKER
 
 # Example:
-# ./extract-rule-test-cases.sh phonology.xfscript| ./rewrite-rule-test.sh phonology.xfscript short hfst | less
+# ./extract-rule-test-cases.sh phonology.xfscript '!!€' | ./rewrite-rule-test.sh phonology.xfscript short hfst | less
 
-cat $1 |
+# cat $1 |
 
-gawk '$1 ~ /^!!€/ {
-  if(test=="")
+gawk -v PHONOLOGY=$1 -v MARKER=$2 'BEGIN { marker=MARKER; phonology=PHONOLOGY;
+  if(phonology=="")
+    phonology="phonology.xfscript";
+       "if [ -f \"" phonology "\" ]\nthen\n echo 1\nelse\necho 0\nfi" | getline exit_status;
+       if(exit_status!=1)
+         {
+           printf "Aborting - Missing phonology file: %s\n", phonology;
+           exit 1;
+         }
+
+  if(marker=="")
+    marker="!!€";
+
+  while((getline < phonology)!=0)
     {
-      test=$2;
-      next;
-    }
-  if(test!="")
-    {
-      test=test "\t" $2;
-      next;
-    }
-}
-{
-  if(test!="")
-    {
-      print test;
-      test="";
+      if($1==marker && test!="")
+        test=test "\t" $2;
+      if($1==marker && test=="")
+        test=$2;
+      if($1!=marker && test!="")
+        {
+          print test;
+          test="";
+        }
     }
 }'
