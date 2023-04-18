c026eee6faed2373d36d3402f51b7c6562024eca
antti.arppe@iki.fi <antti.arppe@iki.fi>
2023-04-18 12:42:53 -0600

    Added script to help in debugging missing specifications of multicharacter symbols.

---
+++ src/fst/incoming/print-space-lexc.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# cat bla_verb_full.lexc |
+
+gawk -v MULTCHAR=$1 'BEGIN { multchar=MULTCHAR; }
+{ sub("!.*$","");
+  if(index($0,"Multichar_Symbols")!=0)
+    { p=1; next; }
+  if(index($0,"LEXICON ")!=0)
+    p=2;
+  if(p==1 && $0!="")
+    mult[$0]=length($0);
+  if(p==2)
+    if($0!="" && match($0,"^LEXICON ")==0)
+      {
+        sub("[^ \t]+[[:space:]]*;[ ]*[^!]*$","");
+        PROCINFO["sorted_in"]="@val_num_desc";
+        for(m in mult)
+          if(mult[m]>=2)
+            {
+              gsub("\\+","\\+",m);
+              gsub("\\^","\\^",m);
+              gsub(m, "[&]");
+            }
+        n=split($0, c, "");
+        printf "%i:", NR;
+        s=" ";
+        for(i=1; i<=n; i++)
+           {
+             if(c[i]=="[")
+               {
+                 printf " ";
+                 s="";
+               }
+             if(c[i]=="]")
+               s=" ";
+             if(c[i]!="[" && c[i]!="]")
+               if(multchar=="")
+                 printf "%s%s", s, c[i];
+               else
+                 if(s==" ")
+                   printf "%s%s", s, c[i];
+                 else
+                   printf "%s", multchar;
+           }
+        printf "\n";
+      }
+}'
