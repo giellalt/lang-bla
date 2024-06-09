e2d6f1b116c94319e1d1378708afe8a0e62a5c84
antti.arppe@iki.fi <antti.arppe@iki.fi>
2024-06-09 13:35:43 -0600

    Revised script to work at all, and with both prefixal and suffixal tags.

--- src/fst/morphology/incoming/resplit/extract-tags.sh
+++ src/fst/morphology/incoming/resplit/extract-tags.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
 
-# extract-flags.sh (LEXC)
+# extract-tags.sh (LEXC)
 
 # Usage:
-#    cat lexicon.lexc | ./extract-flags.sh
+#    cat lexicon.lexc | ./extract-tags.sh
 
 gawk '{
  sub("!.*$","");
- while(match($0,"\\+[^\\+]+",f)!=0)
-      {
-        tag=f[0];
-        gsub("\\+","\\+",tag);
-        sub(tag,""); tags[tag]++;
+ n=split($0,lexc,":");
+ if(n==2)
+   while(match(lexc[1], "([^\\+@]+\\+)|(\\+[^\\+@]+)", f)!=0)
+        {
+          tag=f[0];
+          sub("\\+","\\+",tag);
+          sub(tag, "", lexc[1]);
+          tags[f[0]]++;
       }
 }
 END {
   PROCINFO["sorted_in"]="@ind_str_asc";
-  for(tt in tags) print tt;
+  for(tt in tags)
+     print tt;
 }'
