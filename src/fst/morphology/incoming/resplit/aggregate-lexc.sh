9c11ad31487425657b53613426d0ea5c3e28e06f
antti.arppe@iki.fi <antti.arppe@iki.fi>
2024-11-11 12:58:40 -0700

    Revised code to output LEXC files in a specific order.

--- src/fst/morphology/incoming/resplit/aggregate-lexc.sh
+++ src/fst/morphology/incoming/resplit/aggregate-lexc.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-ls root.lexc noun_prefixes.lexc noun_stems.lexc noun_suffixes.lexc verb_prefixes.lexc prevn.lexc verb_stems.lexc verb_suffixes.lexc particles.lexc pronouns.lexc ../dem.lexc |
+echo 'root.lexc noun_prefixes.lexc noun_stems.lexc noun_suffixes.lexc verb_prefixes.lexc prevn.lexc verb_stems.lexc verb_suffixes.lexc particles.lexc pronouns.lexc ../dem.lexc' |
 
-gawk 'BEGIN { printf "!! Blackfoot morphotax\n\n" > "lexicon.lexc"; }
-{
-  file=$0;
-  while((getline line < file)!=0)
-     printf "%s\n", line >> "lexicon.lexc";
-  printf "\n\n" >> "lexicon.lexc";
+gawk 'BEGIN { printf "!! Blackfoot morphotax -- aggregate of all LEXC files\n\n" > "lexicon.lexc"; }
+{ nf=split($0, files, " ");
+  for(i=1; i<=nf; i++)
+     {
+       while((getline line < files[i])!=0)
+          printf "%s\n", line >> "lexicon.lexc";
+       printf "\n\n" >> "lexicon.lexc";
+     }
 }'
