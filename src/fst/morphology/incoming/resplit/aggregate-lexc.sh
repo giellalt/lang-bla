79b7461c1a45b30b2e4974f64cdec0400b7291e7
antti.arppe@iki.fi <antti.arppe@iki.fi>
2024-11-10 22:26:08 -0700

    Added empty lines between LEXC files.

--- src/fst/morphology/incoming/resplit/aggregate-lexc.sh
+++ src/fst/morphology/incoming/resplit/aggregate-lexc.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-cat root.lexc noun_prefixes.lexc noun_stems.lexc noun_suffixes.lexc verb_prefixes.lexc prevn.lexc verb_stems.lexc verb_suffixes.lexc particles.lexc pronouns.lexc ../dem.lexc > lexicon.lexc
+ls root.lexc noun_prefixes.lexc noun_stems.lexc noun_suffixes.lexc verb_prefixes.lexc prevn.lexc verb_stems.lexc verb_suffixes.lexc particles.lexc pronouns.lexc ../dem.lexc |
+
+gawk 'BEGIN { printf "!! Blackfoot morphotax\n\n" > "lexicon.lexc"; }
+{
+  file=$0;
+  while((getline line < file)!=0)
+     printf "%s\n", line >> "lexicon.lexc";
+  printf "\n\n" >> "lexicon.lexc";
+}'
